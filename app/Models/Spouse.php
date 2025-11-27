7482b25937e214de3f7bc2b706955dc1ccd71103
Sheeerey <rjarevalo9@gmail.com>
2025-11-28 00:09:09 +0800

    test

--- app/Models/Spouse.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-class Spouse extends Model {
-    protected $table = 'spouse';
-    protected $fillable = ['borrower_id','full_name','mobile_number','agency_address','occupation'];
-}
