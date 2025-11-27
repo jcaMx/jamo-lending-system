630fdf7f862d2eb59e3dccd5ca68262d2043a234
kir205 <rafaeljoshsanchez11@gmail.com>
2025-11-27 02:59:50 -0800

    Update Spouse model

--- app/Models/spouse.php
+++ app/Models/spouse.php
@@ -1,25 +1,30 @@
 <?php
-  namespace App\Models;
-  use Illuminate\Database\Eloquent\Model;
 
-  class Spouse extends Model {
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Spouse extends Model
+{
+    use HasFactory;
 
     protected $table = 'spouse';
-    protected $primaryKey = 'ID';
 
     protected $fillable = [
-
-      'first_name',
-      'last_name',
-      'contact_no',
-      'occupation',
-      'position',
-      'agency_address',
-      'borrower_id',
-
+        'first_name',
+        'last_name',
+        'contact_no',
+        'occupation',
+        'position',
+        'agency_address',
+        'borrower_id',
     ];
+    public $timestamps = false;
 
-    public function borrower() {
-      return $this->belongsTo(Borrower::class, 'borrower_id', 'ID');
+
+    public function borrower()
+    {
+        return $this->belongsTo(Borrower::class, 'borrower_id', 'ID');
     }
-  }
+}
