8c1599c8a24d9b48d76e3aa28298d46f206e4620
kir205 <rafaeljoshsanchez11@gmail.com>
2025-11-28 00:25:13 -0800

    try comment all migration

--- database/migrations/2025_11_20_041630_create_holiday.php
+++ database/migrations/2025_11_20_041630_create_holiday.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void
-    {
-        Schema::create('holidays', function (Blueprint $table) {
-            $table->id();
-            $table->string('holiday_name', 50);
-            $table->date('holiday_date');
-            $table->enum('holiday_type', ['Regular','Special','Company'])->default('Regular');
-            $table->string('description', 100)->nullable();
-            $table->boolean('is_Recurring');
-            $table->timestamp('created_at')->useCurrent();
-        });
-    }
+// return new class extends Migration {
+//     public function up(): void
+//     {
+//         Schema::create('holidays', function (Blueprint $table) {
+//             $table->id();
+//             $table->string('holiday_name', 50);
+//             $table->date('holiday_date');
+//             $table->enum('holiday_type', ['Regular','Special','Company'])->default('Regular');
+//             $table->string('description', 100)->nullable();
+//             $table->boolean('is_Recurring');
+//             $table->timestamp('created_at')->useCurrent();
+//         });
+//     }
 
-    public function down(): void
-    {
-        Schema::dropIfExists('holidays');
-    }
-};
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('holidays');
+//     }
+// };
