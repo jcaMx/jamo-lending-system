fd45b87b82454cfee5663121c7e31bbba8d0e0d8
kir205 <rafaeljoshsanchez11@gmail.com>
2026-05-13 13:45:49 -0700

    fix

--- config/mail.php
+++ config/mail.php
@@ -122,8 +122,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'onboarding@resend.dev'),
+        'name' => env('MAIL_FROM_NAME', 'JAMO Lending Corp'),
     ],
 
 ];
