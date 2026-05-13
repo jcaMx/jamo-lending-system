a253e79d51763816f6e64893145b035a031c064e
kir205 <rafaeljoshsanchez11@gmail.com>
2026-05-13 07:17:59 -0700

    fix najud

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -26,40 +26,13 @@ export default function AppLayout({ children, breadcrumbs }: AppLayoutProps) {
     ? SidebarRegistry[matchedRole]
     : CustomerSidebar;
 
-  if (matchedRole === 'customer') {
-    return (
-      <SidebarProvider>
-        <CustomerSidebar />
-        <SidebarInset>
-          <div className="flex h-full flex-col">
-            <TopBar />
-            <main className="flex-1 overflow-auto p-4 lg:p-6">
-              {breadcrumbs && (
-                <div className="mb-4 px-1 text-sm text-gray-500">
-                  {breadcrumbs.map((b, i) => (
-                    <span key={i}>
-                      {b.title}
-                      {i < breadcrumbs.length - 1 && ' / '}
-                    </span>
-                  ))}
-                </div>
-              )}
-              {children}
-            </main>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    );
-  }
-
   return (
     <SidebarProvider className="h-screen w-screen overflow-hidden">
       <SidebarComponent />
       <SidebarInset>
-        <div className="flex h-full w-full bg-gray-50 overflow-hidden flex-col">
+        <div className="flex h-full w-full flex-col overflow-hidden bg-gray-50">
           <TopBar />
-          {/* Page content */}
-          <main className="flex-1 overflow-auto p-6">
+          <main className="flex-1 overflow-auto p-4 lg:p-6">
             {breadcrumbs && (
               <div className="mb-4 text-sm text-gray-500">
                 {breadcrumbs.map((b, i) => (
